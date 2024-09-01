a3af9e539af5ce570352b2b8c12891983b1ede51
kapilsarma2002 <sarmakapil1818@gmail.com>
2024-09-02 00:29:09 +0530

    title and desc change

--- sol-swap-frontend/components/Hero.tsx
+++ sol-swap-frontend/components/Hero.tsx
@@ -13,15 +13,17 @@ const Hero = () => {
   return (
     <div className="p-20">
       <div className="flex items-center justify-start h-full text-5xl font-semibold">
-        Your Gateway to Seamless Token Swapping on&nbsp;
+        Your Personal Digital Asset Vault&nbsp;
         <span className="text-blue-700 font-bold">Solana</span>
       </div>
       <div className="flex items-center justify-center text-2xl text-gray-600 mx-auto">
-        Effortlessly exchange tokens with lightning-fast transactions, minimal
-        fees, and unmatched security on the Solana blockchain.
+        Easily monitor and assess the value of your Solana tokens and NFTs.
       </div>
       <div className="flex items-center justify-center mt-10">
-        <button className="px-4 py-2 bg-blue-700 mt-4 rounded-lg text-white font-bold text-lg flex gap-2" onClick={SignUp}>
+        <button
+          className="px-4 py-2 bg-blue-700 mt-4 rounded-lg text-white font-bold text-lg flex gap-2"
+          onClick={SignUp}
+        >
           <Image src="/google.svg" alt="google.svg" width={24} height={18} />{' '}
           Sign Up with Google
         </button>
