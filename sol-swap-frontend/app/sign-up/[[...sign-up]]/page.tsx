584028bd4c7228fa7acbc29caf199ebfc8eb8b2a
kapilsarma2002 <sarmakapil1818@gmail.com>
2024-09-02 22:02:27 +0530

    deployment commit

--- sol-swap-frontend/app/sign-up/[[...sign-up]]/page.tsx
+++ sol-swap-frontend/app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from '@clerk/nextjs'
 const SignUpPage = () => {
   return (
     <div className="h-screen w-screen bg-white flex justify-center items-center">
-      <SignUp forceRedirectUrl={'/new-user'} />
+      <SignUp forceRedirectUrl={'/new-user'} fallbackRedirectUrl={'/new-user'}/>
     </div>
   )
 }
