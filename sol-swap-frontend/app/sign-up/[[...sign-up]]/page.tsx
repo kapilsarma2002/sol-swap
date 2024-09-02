01711cefcfb96dece3dabf4b36197ab4d8a3a138
kapilsarma2002 <sarmakapil1818@gmail.com>
2024-09-02 22:28:52 +0530

    deployment commit

--- sol-swap-frontend/app/sign-up/[[...sign-up]]/page.tsx
+++ sol-swap-frontend/app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,10 @@ import { SignUp } from '@clerk/nextjs'
 const SignUpPage = () => {
   return (
     <div className="h-screen w-screen bg-white flex justify-center items-center">
-      <SignUp forceRedirectUrl={'/new-user'} fallbackRedirectUrl={'/new-user'}/>
+      <SignUp
+        forceRedirectUrl={'https://sol-swap-smoky.vercel.app/new-user'}
+        fallbackRedirectUrl={'/new-user'}
+      />
     </div>
   )
 }
