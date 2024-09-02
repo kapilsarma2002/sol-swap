4a0b8ce0237dde70b305a1c58a1fb0e5104bca26
kapilsarma2002 <sarmakapil1818@gmail.com>
2024-09-02 23:19:06 +0530

    deployment commit

--- sol-swap-frontend/app/sign-up/[[...sign-up]]/page.tsx
+++ sol-swap-frontend/app/sign-up/[[...sign-up]]/page.tsx
@@ -4,9 +4,7 @@ const SignUpPage = () => {
   return (
     <div className="h-screen w-screen bg-white flex justify-center items-center">
       <SignUp
-        forceRedirectUrl={'https://sol-swap-smoky.vercel.app/new-user'}
-        fallbackRedirectUrl={'/new-user'}
-      />
+        fallbackRedirectUrl={'/new-user'}      />
     </div>
   )
 }
